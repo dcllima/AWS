6fc2859901c7ef307f2ed870b6356e5fdf4ac6ca
diego <dcl_lima@yahoo.com.br>
2020-03-25 02:06:26 -0300

    Spark

---
+++ Teste/S3.py
@@ -0,0 +1,23 @@
+import boto3
+from pyspark.sql import SparkSession
+import pandas as pd
+
+if __name__ == "__main__":
+    client = boto3.client('s3')  # low-level functional API
+
+    # resource = boto3.resource('s3')  # high-level object-oriented API
+    # my_bucket = resource.Bucket('ve-datalake-stage-vg-dev')  # subsitute this for your s3 bucket name.
+
+    '''
+    obj = client.get_object(Bucket='ve-datalake-stage-vg-dev',
+                            Key='gene/contratos/comercial/tmp_aditivos/dt_ref=2020-03-16/Aditivos.csv')
+    
+    fileData = obj['Body'].read()
+    '''
+    sc = SparkSession.Builder().appName("Teste").getOrCreate()
+    csv = pd.read_csv(
+        "s3://ve-datalake-stage-vg-dev/gene/contratos/comercial/tmp_aditivos/dt_ref=2020-03-18/Aditivos.csv")
+
+    #df = sc.createDataFrame(csv,schema=)
+
+    sc.stop()
--- Teste/vai.py
+++ Teste/vai.py
@@ -10,9 +10,4 @@ if __name__ == "__main__":
 
     sc.sql("select * from teste").show()
 
-
-
     sc.stop()
-
-
-
