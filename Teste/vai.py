f8c1a386ac3943cf57d670e4cffda5191e8ae025
diego <dcl_lima@yahoo.com.br>
2020-03-24 21:20:29 -0300

    Spark

--- Teste/vai.py
+++ Teste/vai.py
@@ -1,3 +1,14 @@
 from boto3 import s3
+from docutils.nodes import paragraph
+from pyspark.sql import SparkSession
+
+if __name__ == "__main__":
+    sc = SparkSession.Builder().appName("Teste").getOrCreate()
+    parquetFile = sc.read.parquet("/home/diego/Documentos/parquet.parquet")
+    parquetFile.printSchema()
+
+    view = parquetFile.createOrReplaceTempView("teste")
+
+    sc.sql("select * from teste").show()
 
 
