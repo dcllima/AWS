658276c1b1cace5c403dee3ff149798b3656f4f5
diego <dcl_lima@yahoo.com.br>
2020-03-30 21:12:15 -0300

    Spark

--- Teste/S3.py
+++ Teste/S3.py
@@ -1,6 +1,7 @@
 import boto3
-from pyspark.sql import SparkSession,SQLContext
+from pyspark.sql import SparkSession, SQLContext
 import pandas as pd
+from pyspark.sql.types import StringType, StructField, StructType
 
 if __name__ == "__main__":
     client = boto3.client('s3')  # low-level functional API
@@ -16,9 +17,27 @@ if __name__ == "__main__":
     '''
     sc = SparkSession.Builder().appName("Teste").getOrCreate()
     spark = SQLContext(sc)
+
+    aditivosSchema = StructType([
+        StructField("Id", StringType(), True),
+        StructField("IdContrato", StringType(), True),
+        StructField("IdClausulasInfoGerais", StringType(), True),
+        StructField("IdClausulasPoliticasDatacao", StringType(), True),
+        StructField("IdClausulasParadas", StringType(), True),
+        StructField("Principal", StringType(), True),
+        StructField("Descricao", StringType(), True),
+        StructField("InicioValidade", StringType(), True),
+        StructField("FimValidade", StringType(), True),
+        StructField("InicioVigenciaContrato", StringType(), True),
+        StructField("FimVigenciaContrato", StringType(), True),
+        StructField("DataAssinatura", StringType(), True),
+        StructField("IdArmazenadorCamposSI", StringType(), True),
+        StructField("IdOpcoesFaturamento", StringType(), True)])
+
     csv = pd.read_csv(
         "s3://ve-datalake-stage-vg-dev/gene/contratos/comercial/tmp_aditivos/dt_ref=2020-03-18/Aditivos.csv")
 
-    #df = sc.createDataFrame(csv,schema=)
-
+    df = spark.createDataFrame(csv, schema=aditivosSchema)
+    df.printSchema()
+    df.show()
     sc.stop()
